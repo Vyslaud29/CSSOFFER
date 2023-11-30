233fab2c86f55e16320215d1bcebe0637563ff56
Vyslaud29 <146316187+Vyslaud29@users.noreply.github.com>
2023-11-30 10:39:20 +0100

    Add files via upload

--- main.php
+++ main.php
@@ -3,47 +3,215 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CSOFFER</title>
+    <title>CSSOFFER</title>
     <link rel="stylesheet" href="style.css">  
 </head>
 <body>
     <header>
         <h1>
-            CSOFFER
+            CSSOFFER
         </h1>
     </header>
 
     <div id="lewo">
-    <h1>Ostatnie losowania: </h1>
+    <h1> </h1>
 
-    <?php 
-    // if (isset($_POST['upload'])){
-    //     $target = "zdjecia/KatoChallengers".basename($_FILES['image']['name']);
-        $db = mysqli_connect('localhost', 'root', '', 'csoffer');
-        // $image = $_FILES['image']['name'];
-        // $sql = "SELECT `ibuypowerHolo` FROM `skrzynia2`", ('$image');
-        // mysqli_query($db, $sql);
-        $table = 'skrzynia1';
-    // }
+
+     
     
-    
-    ?>
-    <!-- <form method="post" action="main.php">
-        <input type="hidden" name="img1">
-        <div>
-            <input type="file" name="image">
-        </div>
-        <div>
-            <input type="submit" name="upload" value="upload img">
-        </div>
 
-
-    </form> -->
     </div>
 
 
     <div id="srodek">
         
+
+    <form method="post" action="">
+    <button type="submit" name="otworz1">Otwórz Skrzynię 1</button>
+    <button type="submit" name="otworz2">Otwórz Skrzynię 2</button>
+    <button type="submit" name="otworz3">Otwórz Skrzynię 3</button>
+    <button type="submit" name="otworz4">Otwórz Skrzynię 4</button>
+    <button type="submit" name="otworz5">Otwórz Skrzynię 5</button>
+    <button type="submit" name="otworz6">Otwórz Skrzynię 6</button>
+</form>
+
+    <?php 
+    // // if (isset($_POST['upload'])){
+    // //     $target = "zdjecia/KatoChallengers".basename($_FILES['image']['name']);
+         
+    //     // $image = $_FILES['image']['name'];
+    //     // $sql = "SELECT `ibuypowerHolo` FROM `skrzynia2`", ('$image');
+    //     // mysqli_query($db, $sql);
+    //     $wylosowanaLiczba = rand(1, 10);
+
+    //     $query = "SELECT zdjecie FROM skrzynia1 WHERE id = $wylosowanaLiczba";
+    //     $result = mysqli_query($db, $query);
+
+    //     if ($result->num_rows > 0) {
+    //         $row = $result->fetch_assoc();
+    //         $sciezkaZdjecia = $row['zdjecie'];
+
+            
+    //         echo "<img src='$sciezkaZdjecia' alt='Zdjęcie z skrzyni'>";
+    //     } else {
+    //         echo "Nie udało się pobrać zdjęcia z bazy danych.";
+    //     }
+
+    //     $db->close();
+
+    if (isset($_POST['otworz1'])) {
+        otworzSkrzyne();
+    }
+    
+    function otworzSkrzyne() {
+       
+
+    
+    
+        
+        
+        $wylosowanaLiczba = rand(1, 10);
+        $db = mysqli_connect('localhost', 'root', '', 'cssoffer');
+        $query = "SELECT zdjecie FROM skrzynia1 WHERE id = $wylosowanaLiczba";
+        $result = mysqli_query($db, $query);
+
+
+        if ($wylosowanaLiczba == 1){
+            echo " <h3> Wylosowałeś: Titan (Holo)|Katowice 2014</h3>";
+
+        }
+        if ($wylosowanaLiczba == 2){
+            echo " <h3> Wylosowałeś: Titan |Katowice 2014</h3>";
+        }
+
+        if ($wylosowanaLiczba == 3){
+            echo " <h3> Wylosowałeś: Team Dignitas (Holo)|Katowice 2014</h3>";
+        }
+
+        if ($wylosowanaLiczba == 4){
+            echo " <h3> Wylosowałeś: Team Dignitas|Katowice 2014</h3>";
+        }
+        if ($wylosowanaLiczba == 5){
+            echo " <h3> Wylosowałeś: HellRaisers (Holo)|Katowice 2014</h3>";
+        }
+        if ($wylosowanaLiczba == 6){
+            echo " <h3> Wylosowałeś: HellRaisers|Katowice 2014</h3>";
+        }
+        if ($wylosowanaLiczba == 7){
+            echo " <h3> Wylosowałeś: Fnatic (Holo)|Katowice 2014</h3>";
+        }
+        if ($wylosowanaLiczba == 8){
+            echo " <h3> Wylosowałeś: Fnatic|Katowice 2014</h3>";
+        }
+        if ($wylosowanaLiczba == 9){
+            echo " <h3> Wylosowałeś: compLexity Gaming (Holo)|Katowice 2014</h3>";
+        }
+        if ($wylosowanaLiczba == 10){
+            echo " <h3> Wylosowałeś: compLexity Gaming|Katowice 2014</h3>";
+        }
+
+
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $sciezkaZdjecia = $row['zdjecie'];
+            echo "<img src='$sciezkaZdjecia' alt='Zdjęcie z otwartej skrzyni'>";
+        } else {
+            echo "Nie udało się pobrać zdjęcia z bazy danych.";
+        }
+    
+        $db->close();
+    }
+        ?>
+
+<?php 
+    // // if (isset($_POST['upload'])){
+    // //     $target = "zdjecia/KatoChallengers".basename($_FILES['image']['name']);
+         
+    //     // $image = $_FILES['image']['name'];
+    //     // $sql = "SELECT `ibuypowerHolo` FROM `skrzynia2`", ('$image');
+    //     // mysqli_query($db, $sql);
+    //     $wylosowanaLiczba = rand(1, 10);
+
+    //     $query = "SELECT zdjecie FROM skrzynia1 WHERE id = $wylosowanaLiczba";
+    //     $result = mysqli_query($db, $query);
+
+    //     if ($result->num_rows > 0) {
+    //         $row = $result->fetch_assoc();
+    //         $sciezkaZdjecia = $row['zdjecie'];
+
+            
+    //         echo "<img src='$sciezkaZdjecia' alt='Zdjęcie z skrzyni'>";
+    //     } else {
+    //         echo "Nie udało się pobrać zdjęcia z bazy danych.";
+    //     }
+
+    //     $db->close();
+
+    if (isset($_POST['otworz2'])) {
+        otworzSkrzyne1();
+    }
+    
+    function otworzSkrzyne1() {
+       
+
+    
+    
+        
+        
+        $wylosowanaLiczba = rand(1, 10);
+        $db = mysqli_connect('localhost', 'root', '', 'cssoffer');
+        $query = "SELECT zdjecie FROM skrzynia2 WHERE id = $wylosowanaLiczba";
+        $result = mysqli_query($db, $query);
+
+
+        if ($wylosowanaLiczba == 1){
+            echo " <h3> Wylosowałeś: iBUYPOWER|Katowice 2014</h3>";
+
+        }
+        if ($wylosowanaLiczba == 2){
+            echo " <h3> Wylosowałeś: iBUYPOWER (Holo)|Katowice 2014</h3>";
+        }
+
+        if ($wylosowanaLiczba == 3){
+            echo " <h3> Wylosowałeś: mousesports|Katowice 2014</h3>";
+        }
+
+        if ($wylosowanaLiczba == 4){
+            echo " <h3> Wylosowałeś: mousesports (Holo)|Katowice 2014</h3>";
+        }
+        if ($wylosowanaLiczba == 5){
+            echo " <h3> Wylosowałeś: Natus Vincere|Katowice 2014</h3>";
+        }
+        if ($wylosowanaLiczba == 6){
+            echo " <h3> Wylosowałeś: Natus Vincere (Holo)|Katowice 2014</h3>";
+        }
+        if ($wylosowanaLiczba == 7){
+            echo " <h3> Wylosowałeś: Reason Gaming|Katowice 2014</h3>";
+        }
+        if ($wylosowanaLiczba == 8){
+            echo " <h3> Wylosowałeś: Reason Gaming (Holo)|Katowice 2014</h3>";
+        }
+        if ($wylosowanaLiczba == 9){
+            echo " <h3> Wylosowałeś: Virtus.Pro|Katowice 2014</h3>";
+        }
+        if ($wylosowanaLiczba == 10){
+            echo " <h3> Wylosowałeś: Virtus.Pro (Holo)|Katowice 2014</h3>";
+        }
+
+
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $sciezkaZdjecia = $row['zdjecie'];
+            echo "<img src='$sciezkaZdjecia' alt='Zdjęcie z otwartej skrzyni'>";
+        } else {
+            echo "Nie udało się pobrać zdjęcia z bazy danych.";
+        }
+    
+        $db->close();
+    }
+        ?>
     </div>
 
 
